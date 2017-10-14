b995c425b62206f3a04f9b3af4fff7731e518b47
leticiadesouza <leticia.souza212@gmail.com>
2017-10-14 20:16:28 -0300

    Build Automated

--- atividade6-gulp/project/app/js/library.js
+++ atividade6-gulp/project/app/js/library.js
@@ -2,4 +2,3 @@ function luck_click() {
     var element = document.getElementById("welcomed");
     element.innerHTML = "Play on loto, your numbers are: 4 8 15 16 23 42";
 }
-
---
+++ atividade6-gulp/project/gulpfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+// Gulp watch syntax
+gulp.task('watch', ['clean:dist', 'fonts', 'sass', 'useref', 'images', 'browserSync'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
